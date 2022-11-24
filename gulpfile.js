2335067718c7515e7c1e8e735dafd9dfcb9d7b00
StimoMadness <stimomadness@gmail.com>
2022-11-24 20:57:14 +0300

    added header,about sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const { src, dest, watch, parallel, series } = require('gulp');
-const scss                 = require('gulp-sass')(require('sass'));
-const concat               = require('gulp-concat');
-const autoprefixer         = require('gulp-autoprefixer');
-const uglify               = require('gulp-uglify');
-const imagemin             = require('gulp-imagemin');
-const rename               = require('gulp-rename');
-const nunjucksRender       = require('gulp-nunjucks-render');
-const del                  = require('del');
-const browserSync          = require('browser-sync').create();
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const nunjucksRender = require('gulp-nunjucks-render');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 
 function browsersync() {
   browserSync.init({
-    server:{
+    server: {
       baseDir: 'app/'
     },
     notify: false
@@ -42,17 +42,12 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-    'node_modules/rateyo/src/jquery.rateyo.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js'
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 function images() {
   return src('app/images/**/*.*')
@@ -66,7 +61,7 @@ function images() {
           { cleanupIDs: false }
         ]
       })]))
-  .pipe(dest('dist/images'))
+    .pipe(dest('dist/images'))
 }
 
 function build() {
@@ -74,13 +69,13 @@ function build() {
     'app/**/*.html',
     'app/css/style.min.css',
     'app/js/main.min.js'
-  ],{base:'app'})
-  .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('dist'))
 }
 
 function cleanDist() {
   return del('dist')
-  
+
 }
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
